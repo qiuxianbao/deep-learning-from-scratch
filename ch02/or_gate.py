2eb41772ae84dc8ee1f4255de003ae82216ae563
qiuxianbao <qiuxianbao@aipark.com>
2026-07-21 11:41:56 +0800

    feat(ch02): and, nand, or; xor multi-layered perceptron

--- ch02/or_gate.py
+++ ch02/or_gate.py
@@ -1,20 +1,25 @@
 # coding: utf-8
 import numpy as np
 
-"""
-或门
-"""
+
 def OR(x1, x2):
+    """
+    或门
+    """
     x = np.array([x1, x2])
     w = np.array([0.5, 0.5])
     b = -0.2
-    tmp = np.sum(w*x) + b
+    tmp = np.sum(w * x) + b
     if tmp <= 0:
         return 0
     else:
         return 1
 
+
 if __name__ == '__main__':
     for xs in [(0, 0), (1, 0), (0, 1), (1, 1)]:
         y = OR(xs[0], xs[1])
+        """
+c
+        """
         print(str(xs) + " -> " + str(y))
